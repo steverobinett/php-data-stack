8d6c1102b6780d7e9facbcbbeb62c156906d2709
Steve Robinett <steven.robinett@gfgmsu.edu>
2018-03-28 12:47:29 -0600

    Mods for Mar 28

--- business/getBooks.php
+++ business/getBooks.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="../application/css/books.css">
   <title>Book Listing</title>
 </head>
 <body>
@@ -19,7 +20,7 @@ require('../datastore/db_conn.php');
   $db = bookDb_Connect();
 
   $books = new Books_ds($db);
-  $bookArr = $books->select(NULL); // NULL = 'get all cols'
+  $bookArr = $books->selectAll(NULL); // NULL = 'get all cols'
 
 
   echo buildHTMLTable($bookArr);
@@ -29,15 +30,15 @@ require('../datastore/db_conn.php');
    <?php
   function buildHTMLTable($dataSet){
 
-    $tblHTML = "<table border=1>";
+    $tblHTML = "<table  id='menu' border=1>";
 
     foreach ($dataSet as $row) {
 
-      $tblHTML = $tblHTML.'<tr>';
+      $tblHTML = $tblHTML."<tr id='menu'>";
 
       foreach($row as $col){
 
-        $tblHTML = $tblHTML."<td>$col</td>";
+        $tblHTML = $tblHTML."<td id='menu'>$col</td>";
 
       }
       $tblHTML = $tblHTML."</tr>";
